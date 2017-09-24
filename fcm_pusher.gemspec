61729c315299554fdbda97c78ae231c0ea90874c
Amarildo Lucas <vmarildo@gmail.com>
2017-09-24 12:55:17 -0300

    Refactored and added send_all method

--- fcm_pusher.gemspec
+++ fcm_pusher.gemspec
@@ -1,9 +1,9 @@
 Gem::Specification.new do |s|
   s.name        = 'fcm_pusher'
-  s.version     = '0.0.0'
+  s.version     = '0.0.1'
   s.date        = '2017-09-23'
   s.summary     = "FCM Pusher"
-  s.description = "Send push notifications using the Firebase Cloud Messaging Server"
+  s.description = "A Ruby gem to send push notifications from Firebase Cloud Messaging to Android and iOS devices."
   s.authors     = ["Amarildo Lucas"]
   s.email       = 'vmarildo@gmail.com'
   s.files       = ["lib/fcm_pusher.rb"]
@@ -11,5 +11,5 @@ Gem::Specification.new do |s|
     'http://rubygems.org/gems/fcm_pusher'
   s.license       = 'MIT'
 
-  s.add_development_dependency 'dotenv'
+  s.add_development_dependency 'dotenv', '2.2.1'
 end
--- lib/fcm_pusher.rb
+++ lib/fcm_pusher.rb
@@ -1,4 +1,4 @@
-# This class initializes a push notification object 
+# This class initializes a FcmPusher object 
 # to be sended as a JSON POST request to Google FCM API Server
 # Use the FCM APY_KEY and the FCM_TOKEN from an user or device
 
@@ -7,38 +7,87 @@ require 'json'
 require 'net/https'
 require 'uri'
 
-
 class FcmPusher
-  FCM_BASE_URL = "https://fcm.googleapis.com/fcm/send"
-  FCM_API_KEY = ENV['FCM_API_KEY']
-  attr_accessor :fcm_token 
+  module Priority 
+    HIGH = "high"
+    NORMAL = "normal"
+  end 
 
-  def initialize(fcm_token)
-    @fcm_token = fcm_token 
-  end
+  module Configuration
+    FCM_BASE_URL = "https://fcm.googleapis.com/fcm/send"
+    FCM_API_KEY = "AAAABxYhiPE:APA91bECmJXkhoPKHFqlVA4-y79YABxVMSBUJGMLC35dQwQG-03zCqHmt72r6Pv4eiO42tHb0QyYPW6giAoo8E7KTtZU6IgwA1SH6oK02GNZUgVBI3GwGOrUH1Da8xEqA1d0LjbJrXek"
+  end 
 
-  def send(title, body, icon, sound, badge, priority)
-    uri = URI(FCM_BASE_URL)
+  attr_accessor :fcm_token, :registration_ids, :priority
+  
+  def initialize 
+    @priority = Priority::HIGH
+  end 
+
+  # send notification to a unique registered token
+  # optional parameters are icon, badge, sound and priority
+  def send_once(to, title, body, options = {})
+    @fcm_token = to 
+    set_priority(priority) if !options[:priority].nil? 
+    body = parse_once(to, title, body, options)
+    request(Configuration::FCM_BASE_URL, Configuration::FCM_API_KEY, body)
+  end 
+
+  # send notifications to more than one registered token 
+  # optional parameters are icon, badge, sound and priority
+  def send_all(registration_ids, title, body, options = {})
+    @registration_ids = registration_ids
+    set_priority(priority) if !options[:priority].nil? 
+    body = parse_all(registration_ids, title, body, options)
+    request(Configuration::FCM_BASE_URL, Configuration::FCM_API_KEY, body)
+  end 
+
+  # set the priority of the notification to be sended, the default value is Priority::HIGH
+  def set_priority(priority)
+    @priority = priority
+  end 
+
+  def request(url, api_key, body) 
+    uri = URI(url)
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
-    request = Net::HTTP::Post.new(uri.path, {'Content-Type': 'application/json', 'Authorization': "key=#{FCM_API_KEY}"}) 
-    request.body = json_format(@fcm_token, title, body, icon, sound, badge, priority)
-    response = https.request(request)
+    request = Net::HTTP::Post.new(uri.path, {'Content-Type': 'application/json', 'Authorization': "key=#{api_key}"}) 
+    request.body = body 
+    response = https.request(request) 
     puts "Finished successfully with response #{response}"
   rescue => exception
     puts "Failed with #{exception}"
   end 
 
-  def json_format(to, title, body, icon, sound, badge, priority)
-    { to: to, 
+  # parse json to be used in send_once
+  # optional parameters are icon, badge, sound and priority
+  def parse_once(to, title, body, options = {})
+    { 
+      to: to, 
       notification: { 
         title: title, 
         body: body,
-        icon: icon,
-        badge: badge,
-        sound: sound 
+        icon: options[:icon],
+        badge: options[:badge],
+        sound: options[:sound]
       }, 
-      priority: priority 
+      priority: options[:priority]
     }.to_json
   end 
+
+  # parse json to be used in send_all
+  # optional parameters are icon, badge, sound and priority
+  def parse_all(registration_ids, title, body, options = {})
+    { 
+      registration_ids: registration_ids, 
+      notification: { 
+        title: title, 
+        body: body,
+        icon: options[:icon],
+        badge: options[:badge],
+        sound: options[:sound]
+      }, 
+      priority: options[:priority] 
+    }.to_json
+  end
 end 

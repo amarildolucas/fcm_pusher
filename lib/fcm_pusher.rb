d218afa0732204528f1b28734debe2ea35a723c4
Alexandre Juca <corextechnologies@gmail.com>
2017-09-25 12:12:48 +0100

    Added FcmPusher to puts so it makes it easier to track actions coming from the lib, especially when checking logs .

--- lib/fcm_pusher.rb
+++ lib/fcm_pusher.rb
@@ -1,4 +1,4 @@
-# This class initializes a FcmPusher object 
+# This class initializes a FcmPusher object
 # to be sended as a JSON POST request to Google FCM API Server
 # Use the FCM APY_KEY and the FCM_TOKEN from an user or device
 
@@ -8,86 +8,86 @@ require 'net/https'
 require 'uri'
 
 class FcmPusher
-  module Priority 
+  module Priority
     HIGH = "high"
     NORMAL = "normal"
-  end 
+  end
 
   module Configuration
     FCM_BASE_URL = "https://fcm.googleapis.com/fcm/send"
     FCM_API_KEY = "AAAABxYhiPE:APA91bECmJXkhoPKHFqlVA4-y79YABxVMSBUJGMLC35dQwQG-03zCqHmt72r6Pv4eiO42tHb0QyYPW6giAoo8E7KTtZU6IgwA1SH6oK02GNZUgVBI3GwGOrUH1Da8xEqA1d0LjbJrXek"
-  end 
+  end
 
   attr_accessor :fcm_token, :registration_ids, :priority
-  
-  def initialize 
+
+  def initialize
     @priority = Priority::HIGH
-  end 
+  end
 
   # send notification to a unique registered token
   # optional parameters are icon, badge, sound and priority
   def send_once(to, title, body, options = {})
-    @fcm_token = to 
-    set_priority(priority) if !options[:priority].nil? 
+    @fcm_token = to
+    set_priority(priority) if !options[:priority].nil?
     body = parse_once(to, title, body, options)
     request(Configuration::FCM_BASE_URL, Configuration::FCM_API_KEY, body)
-  end 
+  end
 
-  # send notifications to more than one registered token 
+  # send notifications to more than one registered token
   # optional parameters are icon, badge, sound and priority
   def send_all(registration_ids, title, body, options = {})
     @registration_ids = registration_ids
-    set_priority(priority) if !options[:priority].nil? 
+    set_priority(priority) if !options[:priority].nil?
     body = parse_all(registration_ids, title, body, options)
     request(Configuration::FCM_BASE_URL, Configuration::FCM_API_KEY, body)
-  end 
+  end
 
   # set the priority of the notification to be sended, the default value is Priority::HIGH
   def set_priority(priority)
     @priority = priority
-  end 
+  end
 
-  def request(url, api_key, body) 
+  def request(url, api_key, body)
     uri = URI(url)
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
-    request = Net::HTTP::Post.new(uri.path, {'Content-Type': 'application/json', 'Authorization': "key=#{api_key}"}) 
-    request.body = body 
-    response = https.request(request) 
-    puts "Finished successfully with response #{response}"
+    request = Net::HTTP::Post.new(uri.path, {'Content-Type': 'application/json', 'Authorization': "key=#{api_key}"})
+    request.body = body
+    response = https.request(request)
+    puts "FcmPusher request completed successfully and received the following response #{response}"
   rescue => exception
-    puts "Failed with #{exception}"
-  end 
+    puts "FcmPusher failed with #{exception}, request details: #{request.inspect}"
+  end
 
   # parse json to be used in send_once
   # optional parameters are icon, badge, sound and priority
   def parse_once(to, title, body, options = {})
-    { 
-      to: to, 
-      notification: { 
-        title: title, 
+    {
+      to: to,
+      notification: {
+        title: title,
         body: body,
         icon: options[:icon],
         badge: options[:badge],
         sound: options[:sound]
-      }, 
+      },
       priority: options[:priority]
     }.to_json
-  end 
+  end
 
   # parse json to be used in send_all
   # optional parameters are icon, badge, sound and priority
   def parse_all(registration_ids, title, body, options = {})
-    { 
-      registration_ids: registration_ids, 
-      notification: { 
-        title: title, 
+    {
+      registration_ids: registration_ids,
+      notification: {
+        title: title,
         body: body,
         icon: options[:icon],
         badge: options[:badge],
         sound: options[:sound]
-      }, 
-      priority: options[:priority] 
+      },
+      priority: options[:priority]
     }.to_json
   end
-end 
+end
